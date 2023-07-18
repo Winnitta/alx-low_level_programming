e625c2d96900734a5d994969e8f7e10b99a26492
Winnitta <achiengojwando@gmail.com>
2023-07-18 06:57:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+   *main - Entry Point
+   *Description - 'Print alphabets lowercase then uppercase'
+   *Return: Always 0
+   */
+ int main(void)
+ {
+         int n = 97;
+	 int m=56;
+ 
+         while (n <= 122)
+         {
+                 putchar(n);
+                 
+	 }
+	 While (m <= 90)
+	 {
+		  putchar (m);
+	 
+	 }
+         putchar('\n');
+         return (0);
+ }
