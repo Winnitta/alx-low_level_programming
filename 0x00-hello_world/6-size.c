f761b8ee4d60274629e7d986d8e046f3e87b458f
Winnitta <achiengojwando@gmail.com>
2023-07-18 01:45:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 		long int;
 		long long int;
 		float;
+
 printf("Size of a char: %zu byte(s)\n", (unsigned long) sizeof(char));
 printf("Size of an int: %zu byte(s)\n", (unsigned long) sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long) sizeof(long int));
