d1eb583a8e045218d7c6ff8cd6cf2308fb31fa6e
Winnitta <achiengojwando@gmail.com>
2023-07-23 23:25:44 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,15 @@ int main(void)
 int n = 97;
 int m = 65;
 
-while (n <= 122)
-{
+	while (n <= 122)
+	{	
 	 putchar(n);
-}
+	}
 
-while (m <= 90)
-{
+	while (m <= 90)
+	{
 	 putchar(m);
-}
+	}
 putchar('\n');
 
 return (0);
